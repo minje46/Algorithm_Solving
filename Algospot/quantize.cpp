338721426051ea40b13d60828596b16d9ea87694
minje46 <minjea1701@naver.com>
2020-02-16 20:40:36 +0900

    Dynamic-Programming
    To figure out minimum quantization based on dynamic programming algorithm.
    Explaination is attached as comment(annotation).

--- Algospot/quantize.cpp
+++ Algospot/quantize.cpp
@@ -2,59 +2,54 @@
 #include<algorithm>
 #include<memory.h>
 
+#define MAX 101
 #define INF 987654321
 
 using namespace std;
 
-int N; //수열의 크기
-			//arr[]:양자화해야 할 수열, 정렬 상태
-			//partSum[]:arr[]의 부분합 저장, partSum[i]=A[0]+...A[i]의 합
-			//partSquareSum[]:arr[] 제곱의 부분합 저장, partSquareSum[i]는 arr[0]^2+...arr[i]^2의 합
-int arr[100], partSum[100], partSquareSum[100];
-int cache[100][10];
-//arr를 정렬하고 각 부분합 계산
-void preCalculate(void)
-{
-	sort(arr, arr + N); //정렬
-	partSum[0] = arr[0];
-	partSquareSum[0] = arr[0] * arr[0];
+int N, S;										// N = The number of arrays.				S = The number of splited parts.
+int arr[MAX];								// arr = The memory of input array.
+int sum[MAX];							// sum = The memory of accumulated sum value.		[sum[i]=A[0]+...A[i]]
+int square_sum[MAX];					// square_sum = The memory of squared sum value.	[squar_sum[i] = arr[0]^2+...arr[i]^2]	
+int cache[MAX][10];					// cache = The memory of memoization.
 
-	for (int i = 1; i < N; i++)
+void Pre_process()			// To calculate basic data values.
+{
+	sort(arr, arr + N);						
+	sum[0] = arr[0];						// Initialization.
+	square_sum[0] = pow(arr[0], 2);
+
+	for (int i = 1; i < N; i++)			
 	{
-		partSum[i] = partSum[i - 1] + arr[i];
-		partSquareSum[i] = partSquareSum[i - 1] + arr[i] * arr[i];
+		sum[i] = sum[i - 1] + arr[i];
+		square_sum[i] = square_sum[i - 1] + pow(arr[i], 2);
 	}
 }
 
-//arr[low]...arr[high] 구간을 하나의 숫자로 표현할 때 최소 오차 합 계산
-int minDiffrence(int low, int high)
-{
-	//부분합을 이용해 arr[low]...arr[high]의 합 구함
-	int sum = partSum[high] - (low == 0 ? 0 : partSum[low - 1]);
-	int squareSum = partSquareSum[high] - (low == 0 ? 0 : partSquareSum[low - 1]);
-	//평균을 반올림한 값으로 이 수들을 표현
-	int mean = (int)(0.5 + (double)sum / (high - low + 1)); //반올림
-															//sum(arr[i]-mean)^2를 전개한 결과를 부분합으로 표현
-															//∑(arr[i]-mean)^2 = (high-low+1)*mean^2 - 2*(∑arr[i])*mean + ∑arr[i]^2
-	int result = squareSum - (2 * mean*sum) + (mean*mean*(high - low + 1));
-	return result;
+int Calculation(int low, int high)			// To calculate the difference formula.
+{													// [arr[low] ~ arr[high] 구간을 하나의 숫자로 표현할 때 최소 오차 합 계산.]
+	int part_sum = sum[high] - (low == 0 ? 0 : sum[low - 1]);				// [부분합을 이용해 arr[low]...arr[high]의 합 구함.]
+	int part_squared_sum = square_sum[high] - (low == 0 ? 0 : square_sum[low - 1]);
+	int mean = (int)(0.5 + (double)part_sum / (high - low + 1));				// [반올림.]
+																									// [sum(arr[i]-mean)^2를 전개한 결과를 부분합으로 표현]
+	int res = part_squared_sum - (2 * mean*part_sum) + (pow(mean, 2)*(high - low + 1));
+	return res;									// [∑(arr[i]-mean)^2 = (high-low+1)*mean^2 - 2*(∑arr[i])*mean + ∑arr[i]^2]
 }
 
-int quantize(int from, int parts) //from번째 이후의 숫자들을 parts개의 묶음으로 묶는다
-{
-	//기저 사례:모든 숫자를 다 양자화했을 때
-	if (from == N)
-		return 0;
-	//기저 사례:숫자는 아직 남았는데 더 묶을 수 없을 때 아주 큰 값 반환
-	if (parts == 0)
-		return INF;
-	int &result = cache[from][parts];
-	if (result != -1)
+int Quantize(int idx, int parts)		// To split the array from "idx".	
+{												// [idx번째 이후의 숫자들을 parts개의 묶음으로 묶는다.]
+	if (idx == N)							// Base case.
+		return 0;								// [모든 숫자를 다 양자화했을 때.]
+	if (parts == 0)							// Base case2.
+		return INF;							// [숫자는 아직 남았는데 더 묶을 수 없을 때 아주 큰 값 반환.]
+
+	int &result = cache[idx][parts];
+	if (result != -1)						// Memoization.
 		return result;
+
 	result = INF;
-	//조각의 길이를 변화시켜 가며 최소치 찾음
-	for (int partSize = 1; from + partSize <= N; partSize++)
-		result = min(result, minDiffrence(from, from + partSize - 1) + quantize(from + partSize, parts - 1));
+	for (int k = 1; idx + k <= N; k++)
+		result = min(result, Calculation(idx, idx + k - 1) + Quantize(idx + k, parts - 1));
 	return result;
 }
 
@@ -66,14 +61,16 @@ int main(void)
 	int test_case;
 	cin >> test_case;
 	for (int t = 1; t <= test_case; t++)
-	{
-		int S; 
+	{	// Initialization.
+		memset(cache, -1, sizeof(cache));
+
 		cin >> N >> S;
 		for (int i = 0; i < N; i++)
 			cin >> arr[i];
-		
-		preCalculate();
-		memset(cache, -1, sizeof(cache));
-		cout << quantize(0, S) << endl;
+
+		Pre_process();
+		int answer = Quantize(0, S);
+
+		cout << answer << endl;
 	}
 }
