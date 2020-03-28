2c64c137664c07fa64cce15a68781381b18d527b
minje46 <minjea1701@naver.com>
2020-03-27 23:43:16 -1200

    Mathmetics
    To figure out the maximum number of blocks to put on based on mathmetics.
    Explaination is attached as comment(annotation).

--- Algospot/board_cover2.cpp
+++ Algospot/board_cover2.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<algorithm>
 #include<memory.h>
+#include<vector>
 
 #define MAX 11
 
@@ -10,48 +11,50 @@ int H, W;																// H,W = The size of map.
 char map[MAX][MAX];											// map = The memory of input data.	
 int R, C;																	// R,C = The size of block.
 char block[4][MAX][MAX];										// block = The memory of block.
+vector<vector<pair<int, int>>> vc;								// vc = The memory of each block's relative coordinates.
+int empty_space, block_size;									// empty_space = The total number of empty space.
 int answer;																// answer = The maximum number of blocks to put in map.
 
-void Rotation_90()
+void Rotations(int k)				// To 90 degree rotate from the current blcok.
 {
-	int y = 0, x = 0;
-	int r = C, c = R;
-	for (int i = c - 1; i >= 0; i--)
-	{
-		for (int j = 0; j < r; j++)
-			block[1][j][i] = block[0][y][x++];
-		y++;
-		x = 0;
-	}
-}
-
-void Rotation_180()
-{
-	int y = C - 1, x = 0;
 	int r = R, c = C;
+	if (k % 2 == 0)								// [직사각형의 경우, (r,c)가 90도 회전하면 (c,r)로 바뀌어야 한다.]
+		swap(r, c);
 	for (int i = 0; i < r; i++)
 	{
-		for (int j = 0; j < c; j++)
-			block[2][i][j] = block[1][y--][x];
-		y = C - 1;
-		x++;
+		for (int j = 0; j < c; j++)					// [이전 block의 column 순서를 현재 block의 row 순서로 회전.]
+			block[k][j][r - i - 1] = block[k - 1][i][j];
 	}
 }
 
-void Rotation_270()
+void Rotated_coordinates()			// To figure out the relative block's coordinate.
 {
-	int y = 0, x = 0;
-	int r = C, c = R;
-	for (int i = c - 1; i >= 0; i--)
+	vc.resize(4);
+	for (int k = 0; k < 4; k++)
 	{
-		for (int j = 0; j < r; j++)
-			block[3][j][i] = block[2][y][x++];
-		y++;
-		x = 0;
+		int y = -1, x = -1;
+		for (int i = 0; i < MAX; i++)
+		{
+			for (int j = 0; j < MAX; j++)
+			{
+				if (block[k][i][j] == '#')
+				{
+					if (y == -1)
+					{
+						y = i;					
+						x = j;
+					}
+					vc[k].push_back(make_pair(i - y, j - x));
+				}
+			}
+		}
 	}
+	sort(vc.begin(), vc.end());										// To delete duplicated blocks.
+	vc.erase(unique(vc.begin(), vc.end()), vc.end());		// [Block 회전 후에 동일한 모양인 것들 delete.]	
+	block_size = vc[0].size();
 }
 
-pair<int, int> Coordinate(int y, int x)
+pair<int, int> Coordinate(int y, int x)			// To figure out the next empty space's coordinate.
 {
 	for (int i = y; i < H; i++)
 	{
@@ -60,79 +63,65 @@ pair<int, int> Coordinate(int y, int x)
 			if (map[i][j] == '.')
 				return make_pair(i, j);
 		}
-		x = 0;
+		x = -1;
 	}
 	return make_pair(-1, -1);
 }
 
-bool Check(int y, int x, int k)
+bool Check(int y, int x, int k)				// To check whetehr it is possible to put block on or not.
 {
-	int r = R, c = C;
-	if (k % 2 == 1)
-		swap(r, c);
-
-	for (int i = 0; i < r; i++)
+	for (int r = 0; r < vc[k].size(); r++)
 	{
-		int ny = y + i;
-		for (int j = 0; j < c; j++)
-		{
-			int nx = x + j;
-			if (block[k][i][j] == '#')
-			{
-				if (ny < 0 || ny >= H || nx < 0 || nx >= W)			// Overflow.
-					return false;
+		int ny = y + vc[k][r].first;
+		int nx = x + vc[k][r].second;
 
-				if (map[ny][nx] == '#')									// Wall exists.	
-					return false;
-			}
-		}
+		if (ny < 0 || ny >= H || nx < 0 || nx >= W)			// Overflow.
+			return false;
+
+		if (map[ny][nx] == '#')									// The wall already exists.
+			return false;
 	}
 	return true;
 }
 
-void Convert(int y, int x, int k)
+void Convert(int y, int x, int k, bool flag)			// To attach or detach the block on the map.
 {
-	int r = R, c = C;
-	if (k % 2 == 1)
-		swap(r, c);
-
-	for (int i = 0; i < r; i++)
+	for (int r = 0; r < vc[k].size(); r++)
 	{
-		int ny = y + i;
-		for (int j = 0; j < c; j++)
-		{
-			int nx = x + j;
-			if (ny < 0 || ny >= H || nx < 0 || nx >= W)			// Overflow.
-				continue;
+		int ny = y + vc[k][r].first;
+		int nx = x + vc[k][r].second;
 
-			if (k == -1)
-				map[ny][nx] = '.';
-			else
-			{
-				if (block[k][i][j] == '#')
-					map[ny][nx] = block[k][i][j];
-			}
-		}
+		if (!flag)					// Detach condition.		
+			map[ny][nx] = '.';
+		else						// Attach condition.
+			map[ny][nx] = '#';
 	}
 }
 
-void DFS(int y, int x, int cnt)
+void DFS(int y, int x, int cnt)			// To figure out the maximum blocks to put on.
 {
-	answer = max(answer, cnt);
+	if (empty_space / block_size + cnt < answer)		// Prunning.
+		return;														// [남은 empty space를 모두 block으로 채워도, answer보다 작다면 가지치기.]
+
 	pair<int, int> pr = Coordinate(y, x);
-	if (pr.first == -1 && pr.second == -1)
+	if (pr.first == -1 && pr.second == -1)				// Base case.
+	{																	// [No more empty space to check.]
+		answer = max(answer, cnt);
 		return;
+	}
 
 	for (int k = 0; k < 4; k++)
 	{
-		if (Check(pr.first, pr.second, k))		// 4 방향을 모두 체크하고 range, 방향, visit다 되면,
+		if (Check(pr.first, pr.second, k))					// Whether it is possible to put block on or not.
 		{
-			Convert(pr.first, pr.second, k);
-			DFS(pr.first, pr.second, cnt + 1);
-			Convert(pr.first, pr.second, -1);
+			Convert(pr.first, pr.second, k, true);			// Put the current block on.
+			empty_space -= block_size;						// Decrease the number of empty spcae.
+			DFS(pr.first, pr.second, cnt + 1);				// Exhaustive search.
+			empty_space += block_size;
+			Convert(pr.first, pr.second, k, false);
 		}
 	}
-	DFS(pr.first, pr.second, cnt);
+	DFS(pr.first, pr.second, cnt);							// Exhaustive search.
 }
 
 int main(void)
@@ -143,12 +132,19 @@ int main(void)
 	int test_case;
 	cin >> test_case;
 	for (int t = 1; t <= test_case; t++)
-	{
+	{	// Initialization.
+		empty_space = answer = 0;
+		vc.clear();
+
 		cin >> H >> W >> R >> C;
 		for (int i = 0; i < H; i++)
 		{
 			for (int j = 0; j < W; j++)
+			{
 				cin >> map[i][j];
+				if (map[i][j] == '.')
+					empty_space++;
+			}
 		}
 		for (int i = 0; i < R; i++)
 		{
@@ -156,11 +152,10 @@ int main(void)
 				cin >> block[0][i][j];
 		}
 
-		Rotation_90();
-		Rotation_180();
-		Rotation_270();
+		for (int k = 1; k < 4; k++)
+			Rotations(k);
+		Rotated_coordinates();
 
-		answer = 0;
 		DFS(0, 0, 0);
 
 		cout << answer << endl;
